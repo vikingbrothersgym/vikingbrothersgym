af26c13525353d9c28082142620e6fbec8c9170b
josemifd <josemifd20@gmail.com>
2026-08-13 16:54:34 +0200

    Updates

--- assets/index-BJO56UeO.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index-C-JGp6DF.js";var r=a;export{r as default};
---
+++ assets/index-FdQXP2NU.js
@@ -0,0 +1,1 @@
+import{c as a,a as e,b as t}from"./index-7cvuTprH.js";const r={renderer:a,...e,...t};var n=r;export{n as default};
